c09a09112ed7d5063a7ac5ecc1b698114e29af67
Marcus Bullock <marcusbullock@hotmail.co.uk>
2024-06-27 20:27:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 const jsonServer = require("json-server");
-const cors = require("cors");
-const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
-const middlewares = jsonServer.defaults();
-
-server.use(cors());
-server.use(jsonServer.bodyParser);
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
-});
+server.listen(port);
